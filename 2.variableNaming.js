bc434ed5449a2e8619876498e0eb3718ecc189df
minsu <cocacolazero024@gmail.com>
2022-04-28 00:27:55 +0900

    variable namings

---
+++ 2.variableNaming.js
@@ -0,0 +1,24 @@
+// good naming examples
+let myAge = 20;
+
+let windSound;
+let backgroundMusic;
+
+//bad naming examples
+let number = 20;
+
+let audio1;
+let audio2;
+
+//tips
+
+let audioWind;
+let audioBird;
+let audioBackground;
+
+//above is better than this when you search variables.
+//category + specific name
+
+let windAudio;
+let birdAudio;
+let BackgroundAudio;
