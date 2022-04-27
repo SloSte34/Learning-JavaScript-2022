0a5530bb1ed8c0669d3cbec4c643a45f7c2c8f5e
minsu <cocacolazero024@gmail.com>
2022-04-28 00:26:54 +0900

    datatype-number

--- 3.datatype-number.js
+++ 3.datatype-number.js
@@ -4,3 +4,11 @@ let double = 1.23;
 console.log(integer);
 console.log(negative);
 console.log(double);
+
+console.log(0 / 123); //0
+console.log(123 / 0); //infinity
+console.log(123 / -0); //minus infinity
+console.log(123 / 'a'); //not a number (NAN)
+
+let bigInt = 1231221214124142142141241421214242412421410n;
+console.log(bigInt);
